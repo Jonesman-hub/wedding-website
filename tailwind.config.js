d6ff52ee547cd75cac3ece89f87e9c85bd2adefa
Jonesman-hub <jonahdbuuck@gmail.com>
2025-02-14 22:07:05 +0100

    Update tailwind.config.js more flare
    additional styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,17 +11,27 @@ export default {
         display: ['Playfair Display', 'serif'],
       },
       colors: {
-        beige: {
-          50: '#FDFBF6',
-          100: '#FAF6ED',
-          200: '#F5EDE0',
-          300: '#EFE4D3',
+        sage: {
+          50: '#F5F7F5',
+          100: '#E6EBE6',
+          200: '#D1DBD1',
+          300: '#BDCBBD',
+          400: '#A8BBA8',
+        },
+        champagne: {
+          50: '#FDF8F5',
+          100: '#FAF1EB',
+          200: '#F5E3D7',
+          300: '#F0D5C3',
         },
         charcoal: {
           700: '#2C2C2C',
           800: '#262626',
           900: '#171717',
         },
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
       },
     },
   },
