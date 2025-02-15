b2bbe23c8f04892f823ac92677017edaea30b15b
Jonesman-hub <jonahdbuuck@gmail.com>
2025-02-15 14:04:33 +0100

    Update supabase.js to include secrets

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://tjotrmjpxdbhyxtftftc.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqb3RybWpweGRiaHl4dGZ0ZnRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2MTM0MDgsImV4cCI6MjA1NTE4OTQwOH0.UZiEjbEsVREvd0eb1wm_XaG9ZFbXOiaSXiS12DmfYI8'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
